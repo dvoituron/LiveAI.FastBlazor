e847030eba358fe70f51d2650d198404e79a84e5
Denis Voituron <dvoituron@microsoft.com>
2022-01-31 17:15:55 +0100

    Split demo in multiple pages

---
+++ Source/Components/Button/FluentAnchor.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace LiveAI.FastBlazor.Components
+{
+    public partial class FluentAnchor
+    {
+        [Parameter]
+        public string? Href { get; set; }
+
+        [Parameter]
+        public Appearance? Appearance { get; set; }
+
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object>? AdditionalAttributes { get; set; }
+    }
+}
---
+++ Source/Components/Card/FluentCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace LiveAI.FastBlazor.Components
+{
+    public partial class FluentCard
+    {
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object>? AdditionalAttributes { get; set; }
+    }
+}
